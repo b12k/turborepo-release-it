dbaa6b49c4690340d502897d4f0a8faac9df4956
Bogdan Kolesnyk <bogdan.kolesnyk@gmail.com>
2023-02-20 16:37:04 +0100

    feat: initial

---
+++ configs/release-it/index.js
@@ -0,0 +1,35 @@
+const packageName = process.env.npm_package_name;
+
+module.exports = {
+  plugins: {
+    '@release-it/conventional-changelog': {
+      preset: {
+        name: 'conventionalcommits',
+      },
+      path: '.',
+      gitRawCommitsOpts: {
+        path: '.',
+      },
+    },
+  },
+  npm: {
+    publish: false,
+  },
+  github: {
+    releaseName: ['Release:', packageName, 'v${version}'].join(' '),
+    // release: true,
+  },
+  git: {
+    tagName: [packageName, '-v${version}'].join(''),
+    // push: true,
+    commit: true,
+    commitMessage: ['ci(release):', packageName, 'released version ${version}'].join(' '),
+    requireCleanWorkingDir: true,
+    addUntrackedFiles: true,
+  },
+  hooks: {
+    'after:bump': [
+      'echo "version: ${version}"',
+    ],
+  },
+};
