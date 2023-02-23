fe8564d13577bfb11a9f20013c08565f3013a0a2
Bogdan Kolesnyk <bogdan.kolesnyk@gmail.com>
2023-02-23 01:14:43 +0100

    chore: initial

---
+++ configs/release-it/index.js
@@ -0,0 +1,34 @@
+const packageName = process.env.npm_package_name;
+
+module.exports = {
+  plugins: {
+    '@release-it/conventional-changelog': {
+      preset: {
+        name: 'conventionalcommits',
+      },
+      path: '.',
+      gitRawCommitsOpts: {
+        path: '.',
+      },
+    },
+  },
+  npm: false,
+  github: {
+    releaseName: ['Release:', packageName, 'v${version}'].join(' '),
+    // release: true,
+  },
+  git: {
+    tagName: [packageName, '-v${version}'].join(''),
+    // push: true,
+    commit: true,
+    requireCommits: true,
+    commitArgs: ['--allow-empty'],
+    commitMessage: ['feat(release):', packageName, 'released version ${version}'].join(' '),
+  },
+  hooks: {
+    'after:bump': [
+      'mvm-update -v ${version}',
+      'git add --all'
+    ],
+  },
+};
